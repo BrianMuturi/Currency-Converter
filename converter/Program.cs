4e992f8e14e8fda5dbbcb1e843a90f3b93129955
Brian Muturi <muturibrian18@gmail.com>
2020-12-22 01:37:55 +0100

    Add project files.

---
+++ converter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float dollars, PLN, result, conversion_rate;
+            conversion_rate = 0.27f;
+            Console.Write("Enter amount in PLN: ");
+            PLN = float.Parse(Console.ReadLine());
+            dollars = conversion_rate * PLN;
+            Console.WriteLine("Amount in Dollars is: "+dollars+ "USD");
+            Console.ReadLine();
+            Console.ReadLine();
+
+        }
+    }
+}
